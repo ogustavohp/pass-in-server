b4796d0d3ac3eb90eaae7672e4f6765a53eaa5e0
Gustavo Henrique <gustavohpereira1.6@gmail.com>
2024-04-06 12:30:45 -0300

    add fastify swagger, cors and error handler config

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,43 @@
 import fastify from 'fastify'
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import { createEvent } from './routes/create-event'
 import { registerForEvent } from './routes/register-for-event'
 import { getEvent } from './routes/get-event'
 import { getAttendeeBadge } from './routes/get-attendee-badge'
 import { checkIn } from './routes/check-in'
 import { getEventAttendees } from './routes/get-event-attendees'
+import { errorHandler } from './error-handler'
+import fastifyCors from '@fastify/cors'
 
 const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*', // Change in production
+})
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ['application/json'],
+    produces: ['application.json'],
+    info: {
+      title: 'pass.in',
+      description:
+        'The pass.in API is a versatile tool designed to streamline user management processes within event applications. From user registration to event check-in, pass.in offers a comprehensive suite of functionalities aimed at enhancing attendee experiences. With seamless integration capabilities, developers can effortlessly incorporate pass.in API into their applications, ensuring efficient control and organization of event attendees.',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
@@ -22,6 +49,8 @@ app.register(getAttendeeBadge)
 app.register(checkIn)
 app.register(getEventAttendees)
 
-app.listen({ port: 3333 }).then(() => {
+app.setErrorHandler(errorHandler)
+
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running in http://localhost:3333/')
 })
