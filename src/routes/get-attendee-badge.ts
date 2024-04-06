7fa26f0a1a5537912a96a885e2e48b3422b0a984
Gustavo Henrique <gustavohpereira1.6@gmail.com>
2024-04-06 11:56:36 -0300

    type and validade response getAttendeeBadge route

--- src/routes/get-attendee-badge.ts
+++ src/routes/get-attendee-badge.ts
@@ -11,7 +11,16 @@ export async function getAttendeeBadge(app: FastifyInstance) {
         params: z.object({
           attendeeId: z.coerce.number().int(),
         }),
-        response: {},
+        response: {
+          200: z.object({
+            badge: z.object({
+              name: z.string(),
+              email: z.string().email(),
+              eventTitle: z.string(),
+              checkInUrl: z.string().url(),
+            }),
+          }),
+        },
       },
     },
     async (request, reply) => {
@@ -36,7 +45,18 @@ export async function getAttendeeBadge(app: FastifyInstance) {
         throw new Error('Attendee not found.')
       }
 
-      return reply.status(200).send({ attendee })
+      const baseUrl = `${request.protocol}://${request.hostname}`
+
+      const checkInUrl = new URL(`/attendees/${attendeeId}/check-in`, baseUrl)
+
+      return reply.status(200).send({
+        badge: {
+          name: attendee.name,
+          email: attendee.email,
+          eventTitle: attendee.event.title,
+          checkInUrl: checkInUrl.toString(),
+        },
+      })
     },
   )
 }
